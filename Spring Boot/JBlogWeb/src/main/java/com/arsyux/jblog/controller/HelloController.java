adebae17087789370529b924e2f711418d796e2a
Arsyux <64832619+Arsyux@users.noreply.github.com>
2023-05-20 15:01:45 +0900

    create jsp file

--- Spring Boot/JBlogWeb/src/main/java/com/arsyux/jblog/controller/HelloController.java
+++ Spring Boot/JBlogWeb/src/main/java/com/arsyux/jblog/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.arsyux.jblog.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
@@ -18,5 +19,17 @@ public class HelloController {
 		// http://localhost:8080/image 입력시 이쪽으로 매핑됨
 		System.out.println("이미지 파일이 요청됨");
 		return "redirect:image/Cat.png";
+	}
+
+	@GetMapping("/jsp")
+	public String jsp(Model model) {
+		/*
+		원래 스프링 부트에서 동적 콘텐츠의 위치는 src/main/resources/templates 폴더임.
+		jsp는 스프링 부트에서 지원하는 템플릿이 아니므로 jsp를 위한 별도의 폴더 구조를 만들어야함
+		src/main에 webapp/WEB-INF/jblog 폴더를 생성한 후 jsp파일을 작성함.
+		*/
+		System.out.println("JSP 파일이 요청됨");
+		model.addAttribute("username", "Arsyux");
+		return "hello";
 	}
 }
