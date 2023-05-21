5d33d6a65ef0dbd6781df7b26c899d2fe7f6209b
Arsyux <64832619+Arsyux@users.noreply.github.com>
2023-05-21 16:43:42 +0900

    chap03 Set JPA

--- Spring Boot/JBlogWeb/src/main/java/com/arsyux/jblog/domain/User.java
+++ Spring Boot/JBlogWeb/src/main/java/com/arsyux/jblog/domain/User.java
@@ -1,5 +1,18 @@
 package com.arsyux.jblog.domain;
 
+import java.security.Timestamp;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
+// 교재에는 javax로 되어있으나, 회사가 바뀌면서 이름이 jakarta로 바뀜
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -13,12 +26,41 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
+// @Entity
+// 특정 클래스를 엔티티로 설정할 때 사용함.
+// JPA는 엔티티 클래스로부터 생성된 객체를 기반으로 데이터베이스 연동을 처리함
+@Entity
+// @Table
+// 엔티티 클래스와 테이블을 매핑할 때 사용하며, @Table을 생략하면 엔티티 클래스와 동일한 이름의 테이블이 자동으로 매핑된다.
+// 회원 테이블 이름이 USER가 아닌 USERS이므로 이를 매핑하였음.
+@Table(name = "USERS")
 public class User {
 
-	// pom.xml의 Dependencies에서 Lombok 설정을 삭제했다가 셋업 후 다시 설정하였음.
-	private int id;
-	private String username;
-	private String password;
+	// Warning 해결: pom.xml의 Dependencies에서 Lombok 설정을 삭제했다가 셋업 후 다시 설정하였음.
+
+	// @Id
+	// 기본키에 대응하는 식별자 변수를 의미함
+	@Id
+	// @GeneratedValue
+	// 식별자 변수에 자동으로 증가된 값을 할당한다.
+	// YAML 파일에 설정한 Dialect 클래스에 따라 식별자 값 전략이 자동으로 결정됨.
+	// 예를 들어, OracleDialect는 시퀀스(Sequence) 전략이 적용되고, H2Dialect는 아이덴티티(identity) 전략이 적용됨.
+	// 1부터 시작하여 자동으로 1씩 증가하도록 증가 전략 설정
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private int id; // 회원 일련번호
+
+	@Column(nullable = false, length = 50, unique = true)
+	private String username; // 로그인 아이디
+
+	@Column(length = 100)
+	private String password; // 비밀 번호
+
+	@Column(nullable = false, length = 100)
 	private String email;
-	
-} 
+
+	@Enumerated(EnumType.STRING)
+	private RoleType role;
+
+	@CreationTimestamp // 현재 시간이 기본값으로 등록되도록 설정
+	private Timestamp createDate;
+}
