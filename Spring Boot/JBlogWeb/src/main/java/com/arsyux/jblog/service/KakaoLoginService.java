cee8ed2556d83ef98bbc93c5a37efcce3c77de3f
Arsyux <64832619+Arsyux@users.noreply.github.com>
2023-06-12 18:30:58 +0900

    getUserInfo Service

--- Spring Boot/JBlogWeb/src/main/java/com/arsyux/jblog/service/KakaoLoginService.java
+++ Spring Boot/JBlogWeb/src/main/java/com/arsyux/jblog/service/KakaoLoginService.java
@@ -53,6 +53,18 @@ public class KakaoLoginService {
 		HttpHeaders header = new HttpHeaders();
 		header.add("Authorization", "Bearer" + accessToken);
 		header.add("Content-type", "application/x-www-form-urlencoded;charset=utf-8");
+		
+		// HttpHeader와 HttpBody를 하나의 객체에 담기(body 정보는 생략 가능)
+		HttpEntity<MultiValueMap<String, String>> requestEntity = new HttpEntity<>(header);
+		
+		// RestTemplate을 이용하면 브라우저 없이 HTTP 요청을 처리할 수 있음.
+		RestTemplate restTemplate = new RestTemplate();
+		
+		// HTTP 요청을 POST(GET) 방식으로 실행 -> 문자열로 응답이 들어옴.
+		ResponseEntity<String> responseEntity = restTemplate.exchange("https://kapi.kakao.com/v2/user/me", HttpMethod.POST, requestEntity, String.class);
+		
+		// 카카오 인증 서버가 반환한 사용자 정보
+		return responseEntity.getBody();
 	}
 	
 }
